7946e8bdee8c67c1d225cef85c079557e71f10c9
Trevor Hebert <t.hebert73@gmail.com>
2023-06-23 01:36:39 -0400

    Add momentjs to project. Add basic setup for dashboard chart

--- src/components/AggregateChart.vue
+++ src/components/AggregateChart.vue
@@ -1,11 +1,11 @@
 <template>
   <Card>
     <template #content>
-      <Line
-        id="aggregate_chart"
-        :options="chartOptions"
+      <Chart
+        type="line"
         :data="chartData"
         :style="chartStyles"
+        :options="{ responsive: true, maintainAspectRatio: false }"
       />
     </template>
   </Card>
@@ -13,6 +13,7 @@
 
 <script lang="ts">
 import Card from 'primevue/card';
+import Chart from 'primevue/chart';
 import { Line } from 'vue-chartjs';
 import {
   Chart as ChartJS,
@@ -24,7 +25,10 @@ import {
   PointElement,
   LineElement,
 } from 'chart.js';
-import { defineComponent } from 'vue';
+import { defineComponent, computed, ref } from 'vue';
+import { Expense, GetExpensesWithPaymentsDocument } from '@/graphql/generated';
+import { useQuery } from '@urql/vue';
+import moment from 'moment';
 
 ChartJS.register(
   Title,
@@ -36,48 +40,145 @@ ChartJS.register(
   LinearScale
 );
 
+type IDataSet = {
+  label: string;
+  data: number[];
+  backgroundColor: string;
+  borderColor: string;
+  borderWidth: number;
+};
+
 export default defineComponent({
   name: 'AggregateChart',
-  components: { Line, Card },
-  data() {
-    return {
-      chartData: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-        datasets: [
-          {
-            label: 'Total',
-            data: [213, 232, 249, 239, 225, 191],
-            backgroundColor: 'rgb(75, 192, 192, 0.2)',
-            borderColor: 'rgb(75, 192, 192)',
-          },
-          {
-            label: 'Gas',
-            data: [63, 69, 75, 99, 95, 92],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132)',
-          },
-          {
-            label: 'Hydro',
-            data: [150, 163, 174, 140, 130, 99],
-            backgroundColor: 'rgba(255, 205, 86, 0.2)',
-            borderColor: 'rgba(255, 205, 86)',
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: true,
-      },
-    };
-  },
+  components: { Card, Chart },
 
-  computed: {
-    chartStyles() {
+  setup() {
+    const expenses = ref<Expense[]>([]);
+
+    const expenseColours = [
+      {
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        borderColor: 'rgb(255, 99, 132)',
+      },
+      {
+        backgroundColor: 'rgba(255, 159, 64, 0.5)',
+        borderColor: 'rgb(255, 159, 64)',
+      },
+      {
+        backgroundColor: 'rgba(255, 205, 86, 0.5)',
+        borderColor: 'rgb(255, 205, 86)',
+      },
+      {
+        backgroundColor: 'rgba(75, 192, 192, 0.5)',
+        borderColor: 'rgb(75, 192, 192)',
+      },
+      {
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+        borderColor: 'rgb(54, 162, 235)',
+      },
+      {
+        backgroundColor: 'rgba(153, 102, 255, 0.5)',
+        borderColor: 'rgb(153, 102, 255)',
+      },
+    ];
+    const xAxisDates = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+
+    const datasets = ref<IDataSet[]>([]);
+
+    const mychart = ref(null);
+
+    const query = useQuery({
+      query: GetExpensesWithPaymentsDocument,
+    });
+    const fetchExpenses = async () => {
+      const result = await query.executeQuery();
+
+      expenses.value = result.data.value?.expenses
+        ? result.data.value?.expenses
+        : [];
+
+      const totalData: number[] = [];
+      const totalDataset = {
+        label: 'Total',
+        data: totalData,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        borderColor: 'rgb(0, 0, 0)',
+        borderWidth: 2,
+      };
+
+      expenses.value.forEach((expense, i) => {
+        const label: string = expense.name;
+        const data: number[] = [];
+        const backgroundColor: string = expenseColours[i]
+          ? expenseColours[i].backgroundColor
+          : 'rgba(201, 203, 207, 0.2)';
+        const borderColor: string = expenseColours[i]
+          ? expenseColours[i].backgroundColor
+          : 'rgb(201, 203, 207)';
+
+        expense.payments?.forEach((payment) => {
+          const paidAtDate = moment(payment.paidAt).format('MMMM');
+
+          xAxisDates.forEach(() => {
+            data.push(0);
+            totalDataset.data.push(0);
+          });
+
+          if (xAxisDates.includes(paidAtDate)) {
+            data[xAxisDates.indexOf(paidAtDate)] = payment.amount;
+            totalDataset.data[xAxisDates.indexOf(paidAtDate)] += payment.amount;
+          }
+        });
+
+        if (expense.payments?.length) {
+          datasets.value.push({
+            label: label,
+            data: data,
+            backgroundColor: backgroundColor,
+            borderColor: borderColor,
+            borderWidth: 2,
+          });
+        }
+      });
+
+      datasets.value.push(totalDataset);
+    };
+
+    fetchExpenses();
+
+    const chartStyles = computed(() => {
       return {
         height: '400px',
         width: '100%',
         position: 'relative',
       };
-    },
+    });
+
+    const chartData = computed(() => {
+      return {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: datasets.value,
+      };
+    });
+    return {
+      datasets,
+      mychart,
+      chartData,
+      chartStyles,
+    };
   },
 });
 </script>
